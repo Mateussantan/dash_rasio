ae8217191c8c0643192a543a53782e10c842e4cf
Mateussantan <mateussantana.uni@gmail.com>
2025-08-03 21:58:44 -0300

    inclusão logo-rasio

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Bell } from "lucide-react";
+import { Bell, ImageMinus } from "lucide-react";
 import Image from "next/image";
+import logo from "../../public/images/logotipo-rasio.png";
 import admin from "../../public/images/admin.jpg";
 import bra from "../../public/images/bra.png";
 
@@ -8,6 +9,13 @@ const Header = () => {
   return (
     <header className="bg-[#1e1e1e] shadow-lg border-b border-[#1e1e1e] mx-4 sm:mx-6 lg:mx-8 mt-4 mb-2 rounded-lg">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 flex items-center justify-between">
+        <Image
+            src={logo}
+            alt="logo-rasio"
+            width={150}
+            height={100}
+            className="shadow-md"
+          />
         <h1 className="text-lg sm:text-xl lg:text-2xl font-semibold text-gray-100">
           Dashboard Integração Rasio
         </h1>
