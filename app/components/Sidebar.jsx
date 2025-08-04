f776312b70d1ce98268102c1ed3a786f6c4b61d2
Mateussantan <mateussantana.uni@gmail.com>
2025-08-03 22:27:53 -0300

    ajuste components

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -45,10 +45,10 @@ const Sidebar = () => {
       className={`relative z-10 transition-all duration-300 ease-in-out flex-shrink-0 ${
         isSidebarOpen ? "w-64" : "w-20"
       }`}>
-      <div className="h-full bg-[#1e1e1e] text-white p-4 flex flex-col border-r border-[#2f2f2f]">
+      <div className="h-full bg-[#1e1e1e] backdrop-blur-md p-4 flex flex-col border-r border-[#2f2f2f]">
         <button
           onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          className="p-2 rounded-full hover:bg-[#2f2f2f] transition-colors w-fit self-end">
+          className="p-2 rounded-full hover:bg-[#2f2f2f] transition-colors max-w-fit cursor pointer">
           <Menu size={22} className="text-gray-300" />
         </button>
 
@@ -59,13 +59,13 @@ const Sidebar = () => {
             return (
               <Link key={item.name} href={item.href || "#"}>
                 <div
-                  className={`flex items-center p-3 text-sm font-medium rounded-md hover:bg-[#2f2f2f] transition-colors mb-2 ${
+                  className={`flex items-center p-4 text-sm font-medium rounded-lg hover:bg-[#2f2f2f] transition-colors mb-2 ${
                     pathname === item.href ? "bg-[#2f2f2f]" : ""
                   }`}>
                   {IconComponent ? (
                     <IconComponent
                       size={20}
-                      className="text-gray-300 min-w-[20px]"
+                      className="min-w-[20px] text-gray-300"
                     />
                   ) : (
                     <span className="text-gray-300 font-bold">?</span>
--- app/overview/page.jsx
+++ app/overview/page.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Bell, ContainerIcon, ShieldBan, Truck } from "lucide-react";
+import StatCard from "../components/StatCard"
+import React from "react";
 
 const OverviewPage = () => {
   return (
-    <div>OverviewPage</div>
-  )
-}
+    <div className="flex-1 overflow-auto relative z-10">
+      <main className="max-w-7xl mx-auto py-4 px-4 lg:px-8">
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8">
+          <StatCard name="Integrações" icon={Truck} value="1000" />
+          <StatCard name="Programações de Veículos" icon={ContainerIcon} value="1000" />
+          <StatCard name="OST" icon={ShieldBan} value="1000" />
+          <StatCard name="Containers" icon={Bell} value="1000" />
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default OverviewPage;
